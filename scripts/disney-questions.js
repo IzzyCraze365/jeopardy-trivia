3189550b95a3a4347b4b265562231791698ab82a
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-01-14 17:13:52 -0500

    Adding Disney Questions

---
+++ scripts/disney-questions.js
@@ -0,0 +1,68 @@
+// Custom Disney Questions
+
+const placeholderQuestions = [
+    {
+      category: "",
+      question: "List all of the Wishes people made over the course of the movie Aladdin.",
+      answer: "Aladdin- make me a prince, save my life, free the Genie. Jafar- make me sultan, make me the most powerful sorceror in the world, make Jasmine fall desperate in love with me (not granted), become an all-powerful genie",
+    },
+    {
+      category: "",
+      question: "In Aladdin, what are the wishes Genie cannot grant?",
+      answer: "1- kill someone, 2- make two people fall in love, 3- bring people back from the dead, 4- no extra wishes (ig snay on the wishing for more wishes)",
+    },
+    {
+      category: "",
+      question: "In Alice in Wonderland, what color is the flamingo that the Queen of Hearts uses in the game of croquet?",
+      answer: "Green",
+    },
+    {
+      category: "",
+      question: "In Alice in Wonderland, what does the card stuck in the Mad Hatter's top hat have written on it?",
+      answer: "10/6",
+    },
+    {
+      category: "",
+      question: "What is Bambi's first word?",
+      answer: "Bird",
+    },
+    {
+      category: "",
+      question: "In Beauty and the Beast, what book was Belle given to keep by the Bookseller because it was her favorite?",
+      answer: "Beauty and the Beast",//(ironic, right?)
+    },
+    {
+      category: "",
+      question: "In Cinderella, what does the King dream about before the royal ball?",
+      answer: "Being a grandfather",
+    },
+    {
+      category: "",
+      question: "In Cinderella, what piece of music do Cinderella's step-sisters successfully sing?",
+      answer: "None, they fail at Sing, Sweet Nightengale",
+    },
+    {
+      category: "",
+      question: "What is the name of Dumbo's mouse friend?",
+      answer: "Timothy Q. Mouse",
+    },
+    {
+      category: "",
+      question: "In Frozen, what is the official name of the location where Anna and Kristoff first meet?",
+      answer: "Wandering Oaken's Trading Post and Sauna",
+    },
+    {
+      category: "",
+      question: "In Hercules, who are Hades' two henchmen?",
+      answer: "Pain and Panic",
+    },
+    {
+      category: "",
+      question: "In Home on the Range, How does the villain steal cattle so easily?",
+      answer: "through mind-control yodelling", // I'm not even joking
+    },
+  
+  ];
+  
+  export default placeholderQuestions;
+  
